ff050c91c546646c727eb707ec333d24c1a5e02b
Aswinn3 <aswinselvan3@gmail.com>
2024-05-14 15:37:07 +0530

    change title

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -20,7 +20,7 @@ const Feed = () => {
           setSelectedCategory={setSelectedCategory}
         />
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2022 JSM Media
+          Copyright © 2023 
         </Typography>
       </Box>
       <Box p={2} sx={{overflowY: "auto", height: "90vh", flex: 2}}>
