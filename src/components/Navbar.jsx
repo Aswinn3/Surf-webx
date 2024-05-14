d8ecc7f05c86e2bd9e465ded3534c738c35105d1
Aswinn3 <aswinselvan3@gmail.com>
2024-05-14 15:50:42 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
     <Stack direction="row" alignItems="center" p={2} sx={{ position: "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
       <Link to="/" style={{ display: "flex", alignItems: "center" }}>
         <img src={logo} alt="logo" height={45} />
-        <h2>Surf-Web</h2>
+        <h2>Surf Web X</h2>
       </Link>
       <SearchBar/>
     </Stack>
